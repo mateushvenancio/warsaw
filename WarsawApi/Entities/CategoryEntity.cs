427f07c3789317369d488c1c84223955043acfb9
Mateus <mateus.venancio@basis.com.br>
2024-01-18 12:54:15 -0300

    entities

---
+++ WarsawApi/Entities/CategoryEntity.cs
@@ -0,0 +1,21 @@
+﻿namespace WarsawApi.Entities
+{
+    public class CategoryEntity
+    {
+        public CategoryEntity(int categoryId, string description, int color)
+        {
+            CategoryId = categoryId;
+            Description = description;
+            Color = color;
+        }
+        public CategoryEntity(string description, int color)
+        {
+            Description = description;
+            Color = color;
+        }
+
+        public int CategoryId { get; set; }
+        public string Description { get; set; }
+        public int Color { get; set; }
+    }
+}
---
+++ WarsawApi/Entities/OperationEntity.cs
@@ -0,0 +1,28 @@
+﻿namespace WarsawApi.Entities
+{
+    public class OperationEntity
+    {
+        public OperationEntity(string? description, decimal value, CategoryEntity category, DateTime date)
+        {
+            Description = description;
+            Value = value;
+            Category = category;
+            Date = date;
+        }
+
+        public OperationEntity(int id, string? description, decimal value, CategoryEntity category, DateTime date)
+        {
+            OperationId = id;
+            Description = description;
+            Value = value;
+            Category = category;
+            Date = date;
+        }
+
+        public int OperationId { get; set; }
+        public string? Description { get; set; }
+        public decimal Value { get; set; }
+        public CategoryEntity Category { get; set; }
+        public DateTime Date { get; set; }
+    }
+}
